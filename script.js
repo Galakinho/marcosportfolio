d3d031ff75cf8976b58dd4dfaa68fc07bf938c4d
Galaxx <87338301+Galakinho@users.noreply.github.com>
2023-10-16 15:11:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,31 @@
-var swiper = new Swiper('.swiper-container', {
-  direction: 'horizontal',
-  loop: true,
-  speed: 500,
-  slidesPerView: 3,
-  autoplay: {
-    delay: 6000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
 
 var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
+  effect: "cards",
   grabCursor: true,
+  perSlideOffset: 8,
+  perSlideRotate: 1,
+  slideShadows: true,
+  shadowOffset: 10,
   loop: true,
-  centeredSlides: true,
-  slidesPerView: "1.2",
-  coverflowEffect: {
-    rotate: 220,
-    stretch: 0,
-    depth: 70,
-    modifier: 0.3,
-    slideShadows: true,
-  },
   autoplay: {
-    delay: 3500,
+    delay: 3000,
     disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-    clickable: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
   },
 });
 
-function search_item() {
-  let input = document.getElementById('pesquisar').value
-  input = input.toLowerCase()
-  let x = document.getElementsByClassName('produto')
-  
-  for(i=0; i < x.length; i++) {
-    if(!x[i].innerHTML.toLowerCase().includes(input)){
-      x[i].style.display="none"
-    }
-    else {
-      x[i].style.display='list-item'
-    }
-  }
+
+
+function hideElement() {
+  var element = document.querySelector('.hide');
+  element.classList.add('hidden');
 }
+
+function hideElement1() {
+  var element = document.querySelector('.overlay');
+  element.classList.add('hidden');
+}
+
+
 
 
 
